421cbb0967108738d606432293a41e95ca03e1e9
Somotech <guido.rizzi@studiorizzi.net>
2026-04-10 17:44:07 +0200

    ven 10 apr 2026, 17:44:07, CEST

--- include/10-modbus-tcp.h
+++ include/10-modbus-tcp.h
@@ -1,8 +1,8 @@
-#ifndef __PROTO_10_MOSBUD_TCP_CPP__
-#define __PROTO_10_MOSBUD_TCP_CPP__
+#ifndef __PROTO_10_MODBUS_TCP_CPP__
+#define __PROTO_10_MODBUS_TCP_CPP__
 //Extracted Prototyes
 // ****************************
-// src/10-mosbud-tcp.cpp prototypes
+// src/10-modbus-tcp.cpp prototypes
 // ****************************
     void sendRequest(uint8_t* request, uint16_t length);
     bool receiveResponse(uint8_t* response, uint16_t maxLength, uint16_t& length);
--- include/revision.h
+++ include/revision.h
@@ -1,1 +1,1 @@
-#define REVISION "7641459 Fri Apr 10 17:37:03 2026 +0200" 
+#define REVISION "6a786d4 Fri Apr 10 17:43:03 2026 +0200" 
